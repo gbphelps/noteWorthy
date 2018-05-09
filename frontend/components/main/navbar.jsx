233d1685c973cf17eefa79484aad0cf40fe3d04f
Grant Phelps <gbphelps2@gmail.com>
2018-05-09 15:34:33 -0400

    Add user popup for logout

--- frontend/components/main/navbar.jsx
+++ frontend/components/main/navbar.jsx
@@ -1,15 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export default class NavBar extends React.Component {
+
+
+
+
+class UserSquare extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      active: false
+    };
+    this.toggle = this.toggle.bind(this)
+  }
+
+  status(){
+    return (this.state.active ? '' : 'hidden')
+  }
+
+  toggle(){
+    const active = this.state.active;
+    this.setState({active: !active});
+  }
+
+
+  render(){
+    return(
+      <div className='user-square' onClick={this.toggle}>
+        <div className={'user-popup ' + this.status()}/>
+      </div>
+    );
+  }
+}
+
+
+
+class NavBar extends React.Component {
   constructor(props){
     super(props);
     this.state = {};
   }
 
-
   popup(text,icon){
     return(
-      <div className={icon}>
+      <div className={icon + ' icon'}>
         <div className='popup'>
           <img className='popup-arrow' src={popupTail}/>
           <div className='popup-body'>{text}</div>
@@ -24,20 +58,29 @@ export default class NavBar extends React.Component {
         <img className='icon-elephant' src={elephantSmall}/>
 
         <nav className='upper-nav'>
-          {this.popup('New Note','icon plus')}
-          {this.popup('New Meeting Note', 'icon mtgnote')}
-          {this.popup('Search', 'icon search')}
+          {this.popup('New Note','plus')}
+          {this.popup('New Meeting Note', 'mtgnote')}
+          {this.popup('Search', 'search')}
         </nav>
 
         <nav className='lower-nav'>
-
-          {this.popup('Shortcuts', 'icon star')}
-          {this.popup('Notes', 'icon note')}
-          {this.popup('Notebooks', 'icon notebook')}
-          {this.popup('Tags', 'icon tag')}
-
+          {this.popup('Shortcuts', 'star')}
+          {this.popup('Notes', 'note')}
+          {this.popup('Notebooks', 'notebook')}
+          {this.popup('Tags', 'tag')}
         </nav>
+
+        <UserSquare/>
+
       </div>
     );
   }
 }
+
+const mapState = state => {
+  return {
+    user: state.session
+  };
+};
+
+export default connect(mapState)(NavBar)
