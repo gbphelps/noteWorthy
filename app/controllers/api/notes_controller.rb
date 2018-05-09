1e90c280b35519a6cff978a31ae67a036a22c41e
Grant Phelps <gbphelps2@gmail.com>
2018-05-09 13:44:45 -0400

    Add that notes model I forgot about

--- app/controllers/api/notes_controller.rb
+++ app/controllers/api/notes_controller.rb
@@ -1,10 +1,22 @@
-class NotesController < ApplicationController
+class Api::NotesController < ApplicationController
   def create
+    @note = Note.new(note_params)
+    if @note.save
+      render :show
+    else
+      render json: ['An unexpected error occured. Please try again'], status: 422
+    end
   end
 
   def delete
   end
 
   def update
+  end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:notebook_id,:title,:body)
   end
 end
