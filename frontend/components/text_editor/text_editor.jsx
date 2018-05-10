7c241d4b82a77b67014a36e9718af1ddf560902b
Grant Phelps <gbphelps2@gmail.com>
2018-05-10 12:04:35 -0400

    Add functionality to CreateNote component

--- frontend/components/text_editor/text_editor.jsx
+++ frontend/components/text_editor/text_editor.jsx
@@ -1,20 +1,45 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchNote } from '../../actions/notes'
+import { createNote } from '../../actions/notes'
 
 class TextEditor extends React.Component{
   constructor(props){
     super(props);
-    this.state={}
+    this.state={
+      title:'',
+      body:''
+    }
+    this.handleSubmit=this.handleSubmit.bind(this);
   }
 
   componentDidMount(){
-    this.props.fetchNote(this.props.match.params.noteId)
+    // this.props.fetchNote(this.props.match.params.noteId)
   }
+
+
+  update(field){
+    return e => {
+      this.setState({[field]: e.target.value})
+    };
+  }
+
+  handleSubmit(e){
+    e.preventDefault();
+    this.props.action(this.state);
+  }
+
 
   render(){
     return(
-      <div>Hellow Werld</div>
+      <form onSubmit={this.handleSubmit}>
+        <input type='text'
+          value={this.state.title}
+          onChange={this.update('title')}/>
+        <textarea
+          onChange={this.update('body')}
+          value={this.state.body}/>
+        <button>Submit</button>
+      </form>
     );
   }
 }
@@ -27,7 +52,8 @@ const mapState = (state, ownProps) => {
 
 const mapDispatch = dispatch => {
   return {
-    fetchNote: id => dispatch(fetchNote(id))
+    // fetchNote: id => dispatch(fetchNote(id))
+    action: note => dispatch(createNote(note))
   };
 };
 
