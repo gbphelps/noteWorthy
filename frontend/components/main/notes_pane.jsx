1ba36046a2ad869cc81fdc9bbb1d51e05514069f
Grant Phelps <gbphelps2@gmail.com>
2018-05-11 18:44:02 -0400

    Add rollup delete animation to notes

--- frontend/components/main/notes_pane.jsx
+++ frontend/components/main/notes_pane.jsx
@@ -9,35 +9,51 @@ import { Link, withRouter } from 'react-router-dom'
 
 
 
-const NoteBody = (props) => {
+class NoteBody extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      collapse: ''
+    };
+    this.collapse = this.collapse.bind(this)
+  }
 
-  const crop = props.note.title.length > 15 ?
-      props.note.title.slice(0,15) :
-      props.note.title;
+  crop(){
+    return this.props.note.title.length > 15 ?
+      this.props.note.title.slice(0,15) :
+      this.props.note.title;
+  }
 
-  return(
-    <div className='note-hover-event'>
-    <Link to={`/home/${props.note.id}`}>
-      <div className="note-wrapper">
-        <div className='note-body'>
-          <div className='note-header'>
-            <div className='title'>{crop}</div>
+  collapse(){
+    this.setState({collapse: 'collapse'})
+  }
+
+  render(){
+    return(
+      <div className={`note-hover-event ${this.state.collapse}`}>
+      <Link to={`/home/${this.props.note.id}`}>
+        <div className="note-wrapper">
+          <div className='note-body'>
+            <div className='note-header'>
+              <div className='title'>{this.crop()}</div>
+            </div>
+            <div className='date'>{formatTime(this.props.note.updated_at)}</div>
+            <p className='body-of-note'>{this.props.note.body}</p>
           </div>
-          <div className='date'>{formatTime(props.note.updated_at)}</div>
-          <p className='body-of-note'>{props.note.body}</p>
         </div>
-      </div>
-    </Link>
-    <NoteOptions
-      note = {props.note}
-      deleteNote = {props.deleteNote}/>
-  </div>
-  );
-};
+      </Link>
+      <NoteOptions
+        note = {this.props.note}
+        deleteNote = {this.props.deleteNote}
+        collapse = {this.collapse}/>
+    </div>
+    );
+  }
+}
 
 
 
-
+// props.deleteNote(props.note.id)}}>
 
 let NoteOptions = (props) => {
  return(
@@ -46,7 +62,9 @@ let NoteOptions = (props) => {
      <div className='note-icon note-reminder'></div>
      <div className='note-icon note-star'></div>
      <div className='note-icon note-trash'
-          onClick={(e) => {props.deleteNote(props.note.id)}}>
+          onClick={(e) => {
+            props.collapse();
+            setTimeout(() => props.deleteNote(props.note.id),1000)}}>    
      </div>
    </div>
  );
