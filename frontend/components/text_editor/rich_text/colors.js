b39542139c39b0f501fd949d0c3303691beaea31
Grant Phelps <gbphelps2@gmail.com>
2018-05-14 18:02:40 -0400

    Fix hover bug with z-indexing

--- frontend/components/text_editor/rich_text/colors.js
+++ frontend/components/text_editor/rich_text/colors.js
@@ -37,4 +37,48 @@ export const colorStyleMap = {
   violet: {
     color: 'rgba(127, 0, 255, 1)',
   },
+
+
+
+
+
+
+  red2: {
+    color: 'rgba(255, 0, 0, 1)'
+  },
+
+  pink2: {
+    color: 'rgba(255, 20, 147, 1)'
+  },
+
+  paleVioletRed2: {
+    color: 'rgba(219,112,147,1)'
+  },
+
+  lightPink2: {
+    color: 'rgba(255, 182, 193, 1)'
+  },
+
+  lightSalmon2: {
+    color: 'rgba(255,160,122,1)'
+  },
+
+  orange2: {
+    color: 'rgba(255, 127, 0, 1)',
+  },
+  yellow2: {
+    color: 'rgba(255, 255, 0, 1)',
+  },
+  green2: {
+    color: 'rgba(0, 180, 0, 1)',
+  },
+  blue2: {
+    color: 'rgba(0, 0, 255, 1)',
+  },
+  indigo2: {
+    color: 'rgba(75, 0, 130, 1)',
+  },
+  violet2: {
+    color: 'rgba(127, 0, 255, 1)',
+  },
 };
