47489c68b141994fca85d4fae9ceeeaf32c7bb6a
Grant Phelps <gbphelps2@gmail.com>
2018-05-14 15:49:16 -0400

    Modify DraftJS COLOR example for color swatches

--- frontend/components/text_editor/draft_editor.jsx
+++ frontend/components/text_editor/draft_editor.jsx
@@ -1,55 +1,183 @@
 import React from 'react';
-import { Editor, EditorState, Modifier, RichUtils } from 'draft-js'
+import {Editor, EditorState, Modifier, RichUtils} from 'draft-js'
 
-class TextEditor extends React.Component {
-  constructor(props){
+export default class ColorfulEditorExample extends React.Component {
+
+  constructor(props) {
     super(props);
-    this.state = { editorState: EditorState.createEmpty() };
-
-    this.onChange = (editorState) => this.setState({editorState});
+    this.state = {editorState: EditorState.createEmpty()};
     this.focus = () => this.editor.focus();
-    this.toggleColor = (color) => this._toggleColor(color);
-
-
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-    this._onBoldClick = this._onBoldClick.bind(this);
-    this._onItalicClick = this._onItalicClick.bind(this);
-  }
-
-  _onBoldClick(){
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  }
-
-  _onItalicClick(){
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+    this.onChange = (editorState) => this.setState({editorState});
+    this.toggleColor = (toggledColor) => this._toggleColor(toggledColor);
   }
 
 
-  handleKeyCommand(command, editorState){
-    const newState = RichUtils.handleKeyCommand(editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return 'handled';
+  _toggleColor(toggledColor) {
+    const {editorState} = this.state;
+    const selection = editorState.getSelection();
+    // Let's just allow one color at a time. Turn off all active colors.
+    const nextContentState = Object.keys(colorStyleMap)
+      .reduce((contentState, color) => {
+        return Modifier.removeInlineStyle(contentState, selection, color)
+      }, editorState.getCurrentContent());
+    let nextEditorState = EditorState.push(
+      editorState,
+      nextContentState,
+      'change-inline-style'
+    );
+    const currentStyle = editorState.getCurrentInlineStyle();
+    // Unset style override for current color.
+    if (selection.isCollapsed()) {
+      nextEditorState = currentStyle.reduce((state, color) => {
+        return RichUtils.toggleInlineStyle(state, color);
+      }, nextEditorState);
     }
-    return 'not-handled';
+    // If the color is being toggled on, apply it.
+    if (!currentStyle.has(toggledColor)) {
+      nextEditorState = RichUtils.toggleInlineStyle(
+        nextEditorState,
+        toggledColor
+      );
+    }
+    this.onChange(nextEditorState);
   }
 
 
-
-
-
-  render(){
-    return(
-      <div className='draft-editor'>
-        <div onClick={this._onBoldClick}>BOLD</div>
-        <div onClick={this._onItalicClick}>ITALIC</div>
-        <Editor
-          editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
-          onChange={this.onChange} />
+  render() {
+    const {editorState} = this.state;
+    return (
+      <div style={styles.root}>
+        <ColorControls
+          editorState={editorState}
+          onToggle={this.toggleColor}
+        />
+        <div style={styles.editor} onClick={this.focus}>
+          <Editor
+            customStyleMap={colorStyleMap}
+            editorState={editorState}
+            onChange={this.onChange}
+            placeholder="Write something colorful..."
+            ref={(ref) => this.editor = ref}
+          />
+        </div>
       </div>
     );
   }
 }
 
-export default TextEditor;
+
+
+class StyleButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onToggle = (e) => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+  render() {
+    let style = styles.styleButton;
+    // if (this.props.active) {
+    //   style = {...styles.styleButton, ...colorStyleMap[this.props.style]};
+    // } else {
+    //   style = styles.styleButton;
+    // }
+    return (
+      <div
+        className='color-swatch'
+        onMouseDown={this.onToggle}
+        style={{background: this.props.color}}>
+      </div>
+    );
+  }
+}
+
+
+
+
+var COLORS = [
+  {color: '#ff0000', style: 'red'},
+  {color: '#ff8800', style: 'orange'},
+  {color: '#ffff00', style: 'yellow'},
+  {color: '#00ff00', style: 'green'},
+  {color: '#0000ff', style: 'blue'},
+  {color: '#ff00ff', style: 'indigo'},
+  {color: '00ffff', style: 'violet'},
+];
+
+
+const ColorControls = (props) => {
+  var currentStyle = props.editorState.getCurrentInlineStyle();
+  return (
+    <div style={styles.controls}>
+      {COLORS.map(type =>
+        <StyleButton
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+          color={type.color}
+        />
+      )}
+    </div>
+  );
+};
+
+
+// This object provides the styling information for our custom color
+// styles.
+
+
+const colorStyleMap = {
+  red: {
+    color: 'rgba(255, 0, 0, 1.0)',
+  },
+  orange: {
+    color: 'rgba(255, 127, 0, 1.0)',
+  },
+  yellow: {
+    color: 'rgba(180, 180, 0, 1.0)',
+  },
+  green: {
+    color: 'rgba(0, 180, 0, 1.0)',
+  },
+  blue: {
+    color: 'rgba(0, 0, 255, 1.0)',
+  },
+  indigo: {
+    color: 'rgba(75, 0, 130, 1.0)',
+  },
+  violet: {
+    color: 'rgba(127, 0, 255, 1.0)',
+  },
+};
+
+
+const styles = {
+  root: {
+    fontFamily: '\'Georgia\', serif',
+    fontSize: 14,
+    padding: 20,
+    width: 600,
+  },
+  editor: {
+    borderTop: '1px solid #ddd',
+    cursor: 'text',
+    fontSize: 16,
+    marginTop: 20,
+    minHeight: 400,
+    paddingTop: 20,
+  },
+  controls: {
+    fontFamily: '\'Helvetica\', sans-serif',
+    fontSize: 14,
+    marginBottom: 10,
+    userSelect: 'none',
+  },
+  styleButton: {
+    color: '#999',
+    cursor: 'pointer',
+    marginRight: 16,
+    padding: '2px 0',
+  },
+};
