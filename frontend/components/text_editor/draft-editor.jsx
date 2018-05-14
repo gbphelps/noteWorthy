cfc86dde3898ce6cba19b1451bd57e5dc18455ff
Grant Phelps <gbphelps2@gmail.com>
2018-05-14 13:37:58 -0400

    Commit before significant changes to text editor

---
+++ frontend/components/text_editor/draft-editor.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Editor, EditorState } from draft.js
+
+class TextEditor extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+      <div className='text-editor-pane'>
+        HELLO WORLD
+      </div>
+  }
+}
