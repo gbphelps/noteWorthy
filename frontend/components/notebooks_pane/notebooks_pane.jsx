9476bf6d4065790f1f235913bb72f5a984b26856
Grant Phelps <gbphelps2@gmail.com>
2018-05-17 09:44:06 -0400

    Add notebook search

--- frontend/components/notebooks_pane/notebooks_pane.jsx
+++ frontend/components/notebooks_pane/notebooks_pane.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { toggleNotebooks } from '../../actions/ui'
+import { toggleNotebooks } from '../../actions/ui';
+import { fetchNotebooks } from '../../actions/notebooks';
+import values from 'lodash/values'
 
 class NotebooksPane extends React.Component {
   constructor(props){
     super(props);
     this.state={
       on: false,
-      panelExit: ''
+      panelExit: '',
+      search: ''
     }
+    this.update = this.update.bind(this)
+  }
+
+  componentDidMount(){
+    this.props.fetchNotebooks()
   }
 
   componentWillReceiveProps(nextProps){
     if (!nextProps.active && this.state.on){
       this.setState({panelExit:'panel-exit'});
       setTimeout(()=>this.setState({on: false}),900);
-    }else{
+    }else if (!this.state.on && nextProps.active){
       this.setState({on: true, panelExit: false})
     }
+  }
+
+  update(e){
+      this.setState({search: e.target.value});
+  }
+
+  notebooksList(){
+    return values(this.props.notebooks).map(notebook =>
+      notebook.name.includes(this.state.search) ? (
+        <li key={notebook.id}>
+          {notebook.name}
+        </li>  ) : null )
   }
 
   render(){
@@ -25,7 +45,15 @@ class NotebooksPane extends React.Component {
     return(
       <div className={`notebooks-modular ${this.state.panelExit}`}>
         <div className='notebooks-veil'/>
-        <div className='notebooks-pane'>Hello World</div>
+        <div className='notebooks-pane'>
+          <div>Notebooks</div>
+          <input
+            className='input'
+            placeholder='Search Notebooks'
+            value={this.state.search}
+            onChange={this.update}/>
+          {this.notebooksList()}
+        </div>
       </div>
     )
   }
@@ -33,8 +61,15 @@ class NotebooksPane extends React.Component {
 
 const mapState = state => {
   return {
-    active: state.ui.notebooks
+    active: state.ui.notebooks,
+    notebooks: state.entities.notebooks,
   };
 };
 
-export default connect(mapState)(NotebooksPane);
+const mapDispatch = dispatch => {
+  return {
+    fetchNotebooks: () => dispatch(fetchNotebooks())
+  };
+};
+
+export default connect(mapState, mapDispatch)(NotebooksPane);
