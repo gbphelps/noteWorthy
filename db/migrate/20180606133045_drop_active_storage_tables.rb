1b010aedd94f2525b301e6b01f6232a204d16e22
Grant Phelps <gbphelps2@gmail.com>
2018-06-06 15:28:56 -0400

    Remove unneeded migration

--- db/migrate/20180606133045_drop_active_storage_tables.rb
+++
@@ -1,7 +0,0 @@
-class DropActiveStorageTables < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :active_storage_attachments
-    drop_table :active_storage_blobs
-    drop_table :attached_images
-  end
-end
